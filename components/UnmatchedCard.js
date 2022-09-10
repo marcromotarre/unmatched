88359b0195e56ee02b36ef1ee951d77b014df884
Marc Romo <marc.romo@attackiq.com>
2022-09-10 09:42:48 +0200

    unmatched card

--- components/UnmatchedCard.js
+++ components/UnmatchedCard.js
@@ -11,7 +11,51 @@ function UnmatchedCard({
   discarted,
   discardCard = () => {},
 }) {
-  return <Box>Unmatched Card</Box>;
+  return (
+    <Box sx={{ position: "relative", width: "100%" }}>
+      <Card sx={{ opacity: discarted ? "0.2" : 1, width: "100%" }}>
+        <Stack spacing={1} direction={"row"}>
+          <UnmatchedType type={type} value={value} />
+          {characterName !== "ANY" && (
+            <Box
+              sx={{
+                width: "30px",
+                height: "30px",
+                border: `1px solid ${stringToColor(
+                  characterNames[characterName]?.name
+                )}`,
+                backgroundColor: `${stringToColor(
+                  characterNames[characterName]?.name
+                )}`,
+                borderRadius: "30px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Typography sx={{ color: "#FFFFFF" }}>
+                {stringAvatar(characterNames[characterName]?.name)}
+              </Typography>
+            </Box>
+          )}
+          <Typography>{title}</Typography>
+        </Stack>
+      </Card>
+      <Box
+        onClick={(discarted) => discardCard(!discarted)}
+        sx={{
+          opacity: "1",
+          right: "-10px",
+          top: "calc(50% - 10px)",
+          position: "absolute",
+          width: "20px",
+          height: "20px",
+          backgroundColor: "black",
+          borderRadius: "20px",
+        }}
+      ></Box>
+    </Box>
+  );
 }
 
 export default UnmatchedCard;
