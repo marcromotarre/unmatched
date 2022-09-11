b513fe78b37dabce274e03b6573294f278b0ac9d
Marc Romo <marc.romo@attackiq.com>
2022-09-11 10:27:21 +0200

    translation

--- data/translations.js
+++ data/translations.js
@@ -47,4 +47,448 @@ export const TRANSLATIONS = {
     en: "Pin the Prey",
     es: "Inmoviliza a la presa",
   },
+  "Hiss and Slither": {
+    en: "Hiss and Slither",
+    es: "Repta y sisea",
+  },
+  "The Hounds of Mighty Zeus": {
+    en: "The Hounds of Mighty Zeus",
+    es: "Sabuesos del poderoso zeus",
+  },
+  "Dash": {
+    en: "Dash",
+    es: "Acto Reflejo",
+  },
+  "Clutching Claws": {
+    en: "Clutching Claws",
+    es: "Garras opresivas",
+  },
+  "Second Shot": {
+    en: "Second Shot",
+    es: "Tiro de gracia",
+  },  
+  "Snipe": {
+    en: "Snipe",
+    es: "Precision",
+  },  
+  "Gaze of Stone": {
+    en: "Gaze of Stone",
+    es: "Mirada de piedra",
+  },  
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  },  
+  "Regroup": {
+    en: "Regroup",
+    es: "Recomponer",
+  },  
+  "Winged Frenzy": {
+    en: "Winged Frenzy",
+    es: "Frenesi Alado",
+  },  
+  "A Momentary Glance": {
+    en: "A Momentary Glance",
+    es: "Un vistazo rapido",
+  },  
+  "Riches Beyond Compare": {
+    en: "Riches Beyond Compare",
+    es: "Riquezas sin igual",
+  },  
+  "By Fortune and Fate": {
+    en: "By Fortune and Fate",
+    es: "Por fortuna y destino",
+  },  
+  "Exploit": {
+    en: "Exploit",
+    es: "El momento oportuno",
+  },  
+  "Toil and Danger": {
+    en: "Toil and Danger",
+    es: "Esfuerzo y peligro",
+  },  
+  "Leap Away": {
+    en: "Leap Away",
+    es: "Salto hacia atras",
+  },  
+  "Commanding Impact": {
+    en: "Commanding Impact",
+    es: "Golpe imponente",
+  },  
+  "What Big Ears You Have 🐺": {
+    en: "What Big Ears You Have 🐺",
+    es: "Que orjas mas grandes tienes 🐺",
+  },  
+  "What's That In My Basket? 🌟": {
+    en: "What's That In My Basket? 🌟",
+    es: "¿Que llevo en la cesta? 🌟",
+  },  
+  "Long Have I Sought You ⚔️": {
+    en: "Long Have I Sought You ⚔️",
+    es: "Hace mucho que te busco ⚔️",
+  },  
+  "Never Leave the Path 🐺": {
+    en: "Never Leave the Path 🐺",
+    es: "Nunca te alejes del camino 🐺",
+  },  
+  "What a Terrible Big Mouth You Have ⚔️": {
+    en: "What a Terrible Big Mouth You Have ⚔️",
+    es: "Que Boca mas grande tienes ⚔️",
+  },  
+  "What Big Eyes You Have 🐺": {
+    en: "What Big Eyes You Have 🐺",
+    es: "Que ojos mas grandes tienes 🐺",
+  },  
+  "What Large Hands You Have 🐺": {
+    en: "What Large Hands You Have 🐺",
+    es: "Que manos mas grandes tienes 🐺",
+  },  
+  "Once Upon a Time 🌹": {
+    en: "Once Upon a Time 🌹",
+    es: "Erase una vez 🌹",
+  },  
+  "Stones in the Belly 🌹": {
+    en: "Stones in the Belly 🌹",
+    es: "Piedras en la tripa 🌹",
+  },  
+  "A Grimm Tale 🌹": {
+    en: "A Grimm Tale 🌹",
+    es: "Un cuento macabro 🌹",
+  },  
+  "Into the Woods 🐺🌹⚔️": {
+    en: "Into the Woods 🐺🌹⚔️",
+    es: "Dentro del bosque 🐺🌹⚔️",
+  },  
+  "A Hunter's Eye": {
+    en: "A Hunter's Eye",
+    es: "El ojo del cazador",
+  },  
+  "Disarming Shot": {
+    en: "Disarming Shot",
+    es: "Desarmar",
+  },  
+  "Defenders of Sherwood": {
+    en: "Defenders of Sherwood",
+    es: "Protectores de Sherwood",
+  },  
+  "Snark": {
+    en: "Snark",
+    es: "Prodigio",
+  },  
+  "Wily Fighting": {
+    en: "Wily Fighting",
+    es: "Contrincante habil",
+  },  
+  "Ambush": {
+    en: "Ambush",
+    es: "Emboscada",
+  },  
+  "Highway Robbery": {
+    en: "Highway Robbery",
+    es: "Asalto inesperado",
+  },  
+  "Piercing Shot": {
+    en: "Piercing Shot",
+    es: "A quemarropa",
+  },  
+  "Steal From the Rich": {
+    en: "Steal From the Rich",
+    es: "Robar a los ricos",
+  },  
+  "Larger Than Life": {
+    en: "Larger Than Life",
+    es: "Grande como la vida misma",
+  }, 
+  "Savagery": {
+    en: "Savagery",
+    es: "Salvajismo",
+  }, 
+  "Hoax": {
+    en: "Hoax",
+    es: "Engaño",
+  }, 
+  "Disengage": {
+    en: "Disengage",
+    es: "Evasion",
+  }, 
+  "It's Just Your Imagination": {
+    en: "It's Just Your Imagination",
+    es: "Es solo tu imaginacion",
+  },  
+  "Crash Through the Trees": {
+    en: "Crash Through the Trees",
+    es: "Allanar el bosque",
+  }, 
+  "Jackalope Horns": {
+    en: "Jackalope Horns",
+    es: "Cuernos de lebrilope",
+  }, 
+  "Fatal Struggle": {
+    en: "Fatal Struggle",
+    es: "Lucha a muerte",
+  }, 
+  "The Ancient Heirloom": {
+    en: "The Ancient Heirloom",
+    es: "Espada ancestral",
+  }, 
+  "No Contest Expecteth": {
+    en: "No Contest Expecteth",
+    es: "Indigno rival",
+  },  
+  "The Equal of Grendel": {
+    en: "The Equal of Grendel",
+    es: "El verdugo de grendel",
+  }, 
+  "Epic Poem": {
+    en: "Epic Poem",
+    es: "Poema epico",
+  }, 
+  "Hot for Battle": {
+    en: "Hot for Battle",
+    es: "Listo para la batalla",
+  }, 
+  "The War-King": {
+    en: "The War-King",
+    es: "Rey de la guerra",
+  }, 
+  "Golden Drinking Horn": {
+    en: "Golden Drinking Horn",
+    es: "Cuerno Dorado",
+  },  
+  "Remnant of Valor": {
+    en: "Remnant of Valor",
+    es: "Vestigios de coraje",
+  }, 
+  "Vigor and Courage": {
+    en: "Vigor and Courage",
+    es: "Fuerza y coraje",
+  }, 
+  "The Game is Afoot": {
+    en: "The Game is Afoot",
+    es: "Que comienzce la caza",
+  }, 
+  "Service Revolver": {
+    en: "Service Revolver",
+    es: "Revolver de servicio",
+  }, 
+  "Elementary": {
+    en: "Elementary",
+    es: "Elemental",
+  },  
+  "Eliminate the Impossible": {
+    en: "Eliminate the Impossible",
+    es: "Descarta lo impossible",
+  }, 
+  "Master of Disguise": {
+    en: "Master of Disguise",
+    es: "Maestro del engaño",
+  }, 
+  "Administer Aid": {
+    en: "Administer Aid",
+    es: "Primeros auxilios",
+  }, 
+  "Fixed Point in a Changing Age": {
+    en: "Fixed Point in a Changing Age",
+    es: "Unico punto fijo en una era cambiante",
+  }, 
+  "Confirm Suspicion": {
+    en: "Confirm Suspicion",
+    es: "Confirmar sospecha",
+  },  
+  "Counterpunch": {
+    en: "Counterpunch",
+    es: "Contragolpe",
+  }, 
+  "Deduce Strategy": {
+    en: "Deduce Strategy",
+    es: "proceso de deduccion",
+  }, 
+  "Study Methods": {
+    en: "Study Methods",
+    es: "metodo de investigacion",
+  }, 
+  "Education Never Ends": {
+    en: "Education Never Ends",
+    es: "Nunca se deja de aprender",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  },  
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  },  
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  },  
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  },  
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  },  
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  },  
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  },  
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  },  
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  },  
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
+  "Feint": {
+    en: "Feint",
+    es: "Finta",
+  }, 
 };
